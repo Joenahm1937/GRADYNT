c7cbbad57c26f680504bc65f82c335e336f3ab8a
Torrance Cui <cuitorrance@gmail.com>
2022-01-21 12:02:33 -0800

    created UI and able to get user input

--- gradyent-web-app/src/App.js
+++ gradyent-web-app/src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/header.js';
 import './App.css';
+import UserInput from './components/userInput.js';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <UserInput></UserInput>
     </div>
   );
 }
