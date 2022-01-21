1aa0b7b14aeeff3c3fb7480017405b15ddb533bc
Joseph Nahm <josephnahm1@gmail.com>
2022-01-21 13:45:54 -0800

    fixed typo

--- gradyent-web-app/src/components/userInput.js
+++ gradyent-web-app/src/components/userInput.js
@@ -10,7 +10,7 @@ function UserInput(){
     }
     return (
       <div className="UserInput">
-        <input className = "TextField" type="text" placeholder="serach anything" onChange={e => setSearchWord(e.target.value)} onKeyDown={(e) => submitValue(e) }/>
+        <input className = "TextField" type="text" placeholder="search anything" onChange={e => setSearchWord(e.target.value)} onKeyDown={(e) => submitValue(e) }/>
         {/* <button className = "Submit" onClick={submitValue}>Submit</button> */}
       </div>
     );
