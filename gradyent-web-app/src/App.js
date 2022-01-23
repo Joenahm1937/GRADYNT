94fd7fb6932cc65123afb533b799ec04d13de13e
Torrance Cui <cuitorrance@gmail.com>
2022-01-23 11:54:39 -0800

    small css changes

--- gradyent-web-app/src/App.js
+++ gradyent-web-app/src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/header.js';
 import './App.css';
 import UserInput from './components/userInput.js';
+// import backGroundColor from './components/gradientBG.js';
 
 function App() {
   return (
+    // <div className="App" style = {backGroundColor}>
     <div className="App">
       <Header></Header>
       <UserInput></UserInput>
