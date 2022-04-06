155da7e3ca6289d7dcafad6acbcf5bbd31ac685b
Torrance Cui <cuitorrance@gmail.com>
2022-04-06 14:36:48 -0700

    init twitter-bot

---
+++ twitter-bot/twitter.js
@@ -0,0 +1,12 @@
+const Twit = require('twit');
+
+let Twitter = new Twit({
+    consumer_key:         '...',
+    consumer_secret:      '...',
+    access_token:         '...',
+    access_token_secret:  '...',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL:            true,     // optional - requires SSL certificates to be valid.
+  })
+
+let stream = Twitter.stream('user');
