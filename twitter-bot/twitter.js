325e1dd1f53fcd605a69c33afec3551d84ddd0e9
Torrance Cui <cuitorrance@gmail.com>
2022-04-07 16:22:10 -0700

    connections set, still need to set up streams

--- twitter-bot/twitter.js
+++ twitter-bot/twitter.js
@@ -1,12 +1,28 @@
 const Twit = require('twit');
+const fs = require('fs');
 
-let Twitter = new Twit({
-    consumer_key:         '...',
-    consumer_secret:      '...',
-    access_token:         '...',
-    access_token_secret:  '...',
+//read info from txt file
+const k = fs.readFileSync('twitter-bot/keys.txt', 'utf-8').split("\n")
+
+//create new twitter object
+const Twitter = new Twit({
+    consumer_key:         k[0],
+    consumer_secret:      k[1],
+    access_token:         k[2],
+    access_token_secret:  k[3],
     timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
     strictSSL:            true,     // optional - requires SSL certificates to be valid.
-  })
+});
 
-let stream = Twitter.stream('user');
+// Setting up a user stream, filters tweets by only mentions
+const stream = Twitter.stream('statuses/filter', { track: '@gradyentapp' });
+
+//NEED TO FIX BUG, NOT CONSOLE LOGGING
+stream.on('tweet', function (tweet) {
+  console.log(tweet);
+})
+
+//post example
+// Twitter.post('statuses/update', { status: 'I have awakened.' }, function(err, data, response) {
+//   err ? console.error(err) : console.log(data);
+// })
