1560d1dd5541ac7bc1eb18e25d2b83517d9c5672
Torrance Cui <cuitorrance@gmail.com>
2022-04-09 13:52:25 -0700

    added alt text to png reply

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const fs = require("fs");
 const { createCanvas } = require("canvas");
 
 const puppeteer = require("puppeteer");
-var browser;
-var page;
+let browser;
+let page;
 (async () => {
   browser = await puppeteer.launch();
 })();
@@ -46,24 +46,25 @@ const Twitter = new Twit({
 // Setting up a user stream, filters tweets by only mentions
 const stream = Twitter.stream("statuses/filter", { track: "@gradyntapp" });
 
-//NEED TO FIX BUG, NOT CONSOLE LOGGING
+// Turn on stream to scan for tweets
 stream.on("tweet", async (tweet) => {
   //id_str to get conversation thread
   let { text, user , id_str} = tweet;
   let username = user.screen_name;
+  console.log("received tweet from: " + username);
 
   text = text.replace(/@gradyntapp/g, "").replace(/\n/g, "");
   try {
     const [palette, newPage] = await getColors(text, browser, page);
     page = newPage;
-    console.log(username);
+    console.log(`generating png of ${text}...`);
 
     const WIDTH = 100;
     const HEIGHT = 50;
 
     const canvas = createCanvas(WIDTH, HEIGHT);
     const ctx = canvas.getContext("2d");
-    var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
+    let grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
     grd.addColorStop(0, `rgb(${palette[0].join(',')})`);
     grd.addColorStop(0.25, `rgb(${palette[1].join(',')})`);
     grd.addColorStop(0.5, `rgb(${palette[2].join(',')})`);
@@ -72,14 +73,11 @@ stream.on("tweet", async (tweet) => {
 
     ctx.fillStyle = grd;
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
-    // ctx.fillStyle = "#f2f2f2";
-    // ctx.font = "32px Arial";
-    // ctx.fillText("Hello", 13, 35);
 
     const buffer = canvas.toBuffer("image/png");
     fs.writeFileSync("test.png", buffer);
 
-    var b64content = fs.readFileSync("test.png", { encoding: "base64" });
+    let b64content = fs.readFileSync("test.png", { encoding: "base64" });
 
     // first we must post the media to Twitter
     Twitter.post(
@@ -88,10 +86,9 @@ stream.on("tweet", async (tweet) => {
       function (err, data, response) {
         // now we can assign alt text to the media, for use by screen readers and
         // other text-based presentations and interpreters
-        var mediaIdStr = data.media_id_string;
-        var altText =
-          "Small flowers in a planter on a sunny balcony, blossoming.";
-        var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
+        let mediaIdStr = data.media_id_string;
+        let altText = "gradynt depiction of" + text;
+        let meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
 
         Twitter.post(
           "media/metadata/create",
@@ -99,14 +96,14 @@ stream.on("tweet", async (tweet) => {
           function (err, data, response) {
             if (!err) {
               // now we can reference the media and post a tweet (media will attach to the tweet)
-              var params = {
+              let params = {
                 status: `@${username} `,
                 in_reply_to_status_id: id_str,
                 media_ids: [mediaIdStr],
               };
 
               Twitter.post("statuses/update", params, function (err, data, response) {
-                console.log('posted');
+                console.log('replied to: ' + username);
               });
             }
           }
