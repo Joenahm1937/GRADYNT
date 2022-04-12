87d3c2a761de8f2175224434bd9b779317930d50
Torrance Cui <cuitorrance@gmail.com>
2022-04-11 17:19:26 -0700

    removes react shapes for now

--- client/src/components/colorPalette.js
+++ client/src/components/colorPalette.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import {Circle} from 'react-shapes';
+// import {Circle} from 'react-shapes';
 
 function ColorPalette(props){
     const colorArr = props.colors.split('#');
+    // return (
+    //     <div className = "ColorPalette">
+    //         <Circle r={15} fill={{color: '#' + colorArr[1]}} stroke={{color:'#fff'}} strokeWidth={2} />
+    //         <Circle r={15} fill={{color: '#' + colorArr[2]}} stroke={{color:'#fff'}} strokeWidth={2} />
+    //         <Circle r={15} fill={{color: '#' + colorArr[3]}} stroke={{color:'#fff'}} strokeWidth={2} />
+    //         <Circle r={15} fill={{color: '#' + colorArr[4]}} stroke={{color:'#fff'}} strokeWidth={2} />
+    //         <Circle r={15} fill={{color: '#' + colorArr[5]}} stroke={{color:'#fff'}} strokeWidth={2} />
+    //     </div>
+    // );
     return (
-        <div className = "ColorPalette">
-            <Circle r={15} fill={{color: '#' + colorArr[1]}} stroke={{color:'#fff'}} strokeWidth={2} />
-            <Circle r={15} fill={{color: '#' + colorArr[2]}} stroke={{color:'#fff'}} strokeWidth={2} />
-            <Circle r={15} fill={{color: '#' + colorArr[3]}} stroke={{color:'#fff'}} strokeWidth={2} />
-            <Circle r={15} fill={{color: '#' + colorArr[4]}} stroke={{color:'#fff'}} strokeWidth={2} />
-            <Circle r={15} fill={{color: '#' + colorArr[5]}} stroke={{color:'#fff'}} strokeWidth={2} />
+        <div>
+            hello
         </div>
-    );
+    )
 }
 
 export default ColorPalette;
